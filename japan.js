4c0f9a34588872503fed2170185d168e876a8e04
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2022-03-23 14:26:07 +0600

    Add files via upload

---
+++ japan.js
@@ -0,0 +1,57 @@
+ {  padding:0;
+    margin:0;
+    box-sizing :  border-box;
+    font-family: lato,-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;}
+    body{ background-image: url('https://ocs.alatoo.edu.kg/pluginfile.php/124428/mod_resource/content/1/hero-background%20photo.png'); background-size: 100%;}
+    #container{
+    max-width: 1060px;
+    margin:auto;}
+    li{
+    text-decoration: none;
+    color: white;
+    margin-left:82px;
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;}
+    ul{ list-style-type: none;
+    display: flex; }
+    #header{ display: flex;
+    justify-content: space-between;
+    margin-top: 56px;
+    align-items: center;
+    margin-bottom: 10%;
+    } 
+    .intro-title{
+    font-style: normal;
+    font-weight: 500;
+    font-size: 50px;
+    line-height: 60px;
+    color: #FFFFFF;
+    max-width: 620px;
+    margin-bottom: 1%;
+    }
+    .intro{
+    max-width:546px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color:white;
+    margin-bottom: 2%;
+    }
+    .vvod-text{
+    background-color: transparent;
+    color:#FFFFFF;
+    border:0;
+    margin: 23px;
+    
+    }
+    .vvod-text:active{
+    border:0;
+    }
+    #vvod{
+    border-bottom: 1px solid white;
+    max-width: 300px;
+    }
